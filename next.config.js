e0b9ac28334d69f59268b22f1eb787285b389bfe
hitsufilm <hitsufilm@gmail.com>
2025-02-12 17:00:59 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-// next.config.js
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '/src',
+    }
+    return config
+  },
 }
+
+module.exports = nextConfig
