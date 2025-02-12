ec416c3c95bb2d0a116c4954f824988e9d357e50
hitsufilm <hitsufilm@gmail.com>
2025-02-12 17:51:39 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   plugins: {
-    'tailwindcss': {},
-    'autoprefixer': {},
-    'postcss-preset-env': {
-      stage: 3,
-      features: {
-        'nesting-rules': true
-      }
-    }
-  }
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
