8dee3b81c127092e6672ea8a5328fbf6b5dcf51c
hitsufilm <hitsufilm@gmail.com>
2025-02-12 17:09:01 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,14 @@
+import Head from 'next/head'
 import TestimonyForm from '../components/TestimonyForm'
 
 export default function Home() {
   return (
     <div>
-      <head>
+      <Head>
         <title>Témoignages Hitsufilm</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
-      <div>
-        <TestimonyForm />
-      </div>
+      </Head>
+      <TestimonyForm />
     </div>
   )
 }
